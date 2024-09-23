d5da828edc3b9eb8acd7d58c352d02bfd383d049
Volkov <volkovyegorandriiovich@gmail.com>
2024-09-24 01:56:17 +0300

    Initial Code Review

--- install-app.sh
+++ install-app.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# Script to silently install and start the todo web app on the virtual machine. 
+# Script to silently install and start the todo web app on the virtual machine.
 # Note that all commands bellow are without sudo - that's because extention mechanism 
 # runs scripts under root user. 
 
-# install system updates and isntall python3-pip package using apt. '-yq' flags are 
-# used to suppress any interactive prompts - we won't be able to confirm operation 
+# install system updates and isntall python3-pip package using apt. '-yq' flags are
+# used to suppress any interactive prompts - we won't be able to confirm operation
 # when running the script as VM extention.  
-apt-get update -yq
-apt-get install python3-pip -yq
+apt update -yq
+apt install python3-pip -yq
 
 # Create a directory for the app and download the files. 
 mkdir /app 
 # make sure to uncomment the line bellow and update the link with your GitHub username
-git clone https://github.com/mate-academy/azure_task_18_configure_load_balancing.git
+cd ~
+git clone https://github.com/YegorVolkov/azure_task_18_configure_load_balancing.git
 cp -r azure_task_18_configure_load_balancing/app/* /app
+
+# ensure it will actually work
+cd /app
+chmod +x start.sh
 
 # create a service for the app via systemctl and start the app
 mv /app/todoapp.service /etc/systemd/system/
